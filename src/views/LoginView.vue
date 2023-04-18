0d92fcd5ee40461a52f0ffc71262aec144c6fef2
Shaga Sresthaa <shagasresthaa@gmail.com>
2023-04-18 23:46:20 +0530

    working on login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+    <LoginPage></LoginPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LoginPage from '@/components/LoginPage.vue'; 
+
+export default defineComponent({
+  name: 'LoginView',
+  components: {LoginPage},
+});
+</script>
