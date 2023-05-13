1d496e70f18c444a31f6cdb2d7fe9ec003c7760a
Shaga Sresthaa <shagasresthaa@gmail.com>
2023-05-13 11:22:18 +0530

    home page and login work in progress

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/styles.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 createApp(App).use(store).use(router).mount("#app");
